f1ac1789e59ce14987e3292b60b2da8819b4814f
florian kinder <florian.kinder@mcl.de>
2019-03-01 19:56:39 +0100

    Added human chunk draft

---
+++ stringx/chunk.go
@@ -0,0 +1,60 @@
+package stringx
+
+import (
+	"strings"
+)
+
+func ChunkHuman(str string, rows, chars int) ([]string, bool) {
+	if chars < 0 && rows < 0 {
+		return []string{str}, false
+	}
+
+	lines := strings.Split(str, "\n")
+	charsOverflow := false
+	charCount := 0
+	for _, v := range lines {
+		charCount += len(v)
+		if len(v) > chars {
+			charsOverflow = true
+		}
+	}
+
+	if len(lines) <= rows && !charsOverflow {
+		return lines, false
+	}
+
+	noNewLineString := strings.Replace(strings.Replace(str, "\r", "", -1), "\n", " ", -1)
+	var chunks []string
+	if charCount > rows*chars {
+		for i := 0; i < len(noNewLineString); i += chars {
+			nn := i + chars
+			if nn > len(noNewLineString) {
+				nn = len(noNewLineString)
+			}
+			chunks = append(chunks, string(noNewLineString[i:nn]))
+			if len(chunks) == rows {
+				break
+			}
+		}
+		return chunks, true
+	}
+
+	buffer := ""
+	for i, v := range strings.Split(noNewLineString, " ") {
+		if len(buffer) + 1 + len(v) > chars {
+			chunks = append(chunks, buffer)
+			if len(chunks) == rows {
+				break
+			}
+			buffer = ""
+		} else if i > 0 {
+			buffer += " "
+		}
+		buffer += v
+	}
+	if buffer != "" {
+		chunks = append(chunks, buffer)
+	}
+
+	return chunks, false
+}
---
+++ stringx/chunk_test.go
@@ -0,0 +1,118 @@
+package stringx
+
+import (
+	"github.com/influxdata/influxdb/pkg/testing/assert"
+	"testing"
+)
+
+func TestChunkHuman(t *testing.T) {
+	t.Run("nolimit singleline", func(t *testing.T) {
+		chunk, truncated := ChunkHuman(
+			`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
+			-1,
+			-1,
+			)
+		assert.Equal(t, truncated, false)
+		assert.Equal(t, chunk, []string{
+			"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+		})
+	})
+	t.Run("nolimit multiline", func(t *testing.T) {
+		chunk, truncated := ChunkHuman(
+			`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
+			-1,
+			-1,
+		)
+		assert.Equal(t, truncated, false)
+		assert.Equal(t, chunk, []string{
+			`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
+		})
+	})
+	t.Run("fit singleline", func(t *testing.T) {
+		chunk, truncated := ChunkHuman(
+			`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
+			1,
+			130,
+		)
+		assert.Equal(t, truncated, false)
+		assert.Equal(t, chunk, []string{
+			"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+		})
+	})
+	t.Run("fit multiline", func(t *testing.T) {
+		chunk, truncated := ChunkHuman(
+			`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
+			3,
+			130,
+		)
+		assert.Equal(t, truncated, false)
+		assert.Equal(t, chunk, []string{
+			"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+			"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+			"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+		})
+	})
+	t.Run("overflow multiline", func(t *testing.T) {
+		chunk, truncated := ChunkHuman(
+			`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyamaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyamaaaaaaaaaaaaaaaaaaaaaaaaaaa`,
+			2,
+			130,
+		)
+		assert.Equal(t, truncated, true)
+		assert.Equal(t, chunk, []string{
+			"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut ",
+		})
+	})
+	t.Run("10x10 fit", func(t *testing.T) {
+		chunk, truncated := ChunkHuman(
+			`Lorem ipsum d
+Lorem ipsum dolor sit`,
+			10,
+			10,
+		)
+		assert.Equal(t, truncated, false)
+		assert.Equal(t, chunk, []string{
+			"Lorem",
+			"ipsum d",
+			"Lorem",
+			"ipsum",
+			"dolor sit",
+		})
+	})
+	t.Run("nolimit singleline", func(t *testing.T) {
+		chunk, truncated := ChunkHuman(
+			`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
+			3,
+			50,
+		)
+		assert.Equal(t, truncated, false)
+		assert.Equal(t, chunk, []string{
+			"Lorem ipsum dolor sit amet, consetetur sadipscing",
+			"elitr, sed diam nonumy eirmod tempor invidunt ut",
+			"labore et dolore magna aliquyam",
+		})
+	})
+	t.Run("nolimit singleline", func(t *testing.T) {
+		chunk, truncated := ChunkHuman(
+			`Bestellanforderungsnummer 1152149880
+ LR-Nr. 6102528
+ Kostenstelle 20000520`,
+			4,
+			35,
+		)
+		assert.Equal(t, truncated, false)
+		assert.Equal(t, chunk, []string{
+			"Lorem ipsum dolor sit amet, consetetur sadipscing",
+			"elitr, sed diam nonumy eirmod tempor invidunt ut",
+			"labore et dolore magna aliquyam",
+		})
+	})
+}
