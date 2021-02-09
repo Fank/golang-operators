13bea9352b14da8b36ec807b82580bf76727b7c5
Florian Kinder <florian.kinder@mcl.de>
2021-02-09 15:46:05 +0100

    wip

--- ioutilx/readn.go
+++ ioutilx/readn.go
@@ -2,33 +2,17 @@ package ioutilx
 
 import (
 	"io"
+	"io/ioutil"
 )
 
 // ReadN reads an given amount of bytes and returns it.
-func ReadN(r io.Reader, n int) ([]byte, bool, error) {
-	var s []byte
-	size := 0
-
-	buf := make([]byte, 1024)
-	for {
-		v, err := r.Read(buf)
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return nil, false, err
-		}
-
-		s = append(s, buf...)
-		size += v
-		if size >= n {
-			break
-		}
+func ReadN(r io.Reader, n int64) ([]byte, bool, error) {
+	b, err := ioutil.ReadAll(io.LimitReader(r, n+1))
+	if err != nil {
+		return nil, false, err
 	}
-
-	index := size
-	if size > n {
-		index = n
+	if int64(len(b)) > n {
+		return b[:n], true, nil
 	}
-
-	return s[:index], size > n, nil
+	return b, false, nil
 }
--- ioutilx/readn_test.go
+++ ioutilx/readn_test.go
@@ -30,20 +30,28 @@ func TestReadN(t *testing.T) {
 		b, truncate, err := ReadN(bytes.NewReader([]byte("")), 2)
 		assert.NoError(t, err)
 		assert.Equal(t, truncate, false)
-		assert.Equal(t, []byte(nil), b)
+		assert.Equal(t, []byte{}, b)
 	})
 	t.Run("nilin2out", func(t *testing.T) {
 		b, truncate, err := ReadN(bytes.NewReader(nil), 2)
 		assert.NoError(t, err)
 		assert.Equal(t, truncate, false)
-		assert.Equal(t, []byte(nil), b)
+		assert.Equal(t, []byte{}, b)
 	})
-	t.Run("nilin2out", func(t *testing.T) {
+	t.Run("8192in5000out", func(t *testing.T) {
 		s := bytes.Repeat([]byte("a"), 8192)
 		b, truncate, err := ReadN(bytes.NewReader(s), 5000)
 		assert.NoError(t, err)
 		assert.Equal(t, truncate, true)
 		assert.Equal(t, 5000, len(b))
 		assert.Equal(t, b, s[:5000])
+	})
+	t.Run("3000lin5000out", func(t *testing.T) {
+		s := bytes.Repeat([]byte("abc"), 1000)
+		b, truncate, err := ReadN(bytes.NewReader(s), 5000)
+		assert.NoError(t, err)
+		assert.Equal(t, truncate, false)
+		assert.Equal(t, 3000, len(b))
+		assert.Equal(t, b, s)
 	})
 }
